3384018b9d252632c34151f00029c63688ebfac5
Ethan Nguyen <nethang4@gmail.com>
2025-02-18 14:56:06 -0800

    Created admin page

--- Areas/Identity/Pages/Account/Admin.cshtml.cs
+++ Areas/Identity/Pages/Account/Admin.cshtml.cs
@@ -1,12 +1,24 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Assignment1.Models;
 
 namespace Assignment1.Areas.Identity.Pages.Account
 {
     public class AdminModel : PageModel
     {
-        public void OnGet()
+        private readonly UserManager<User> _userManager;
+
+        public AdminModel(UserManager<User> userManager)
         {
+            _userManager = userManager;
+        }
+
+        public IList<User> Users { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Users = _userManager.Users.ToList();
         }
     }
 }
